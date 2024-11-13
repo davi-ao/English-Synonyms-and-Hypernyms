5d5651a082539d9597a343f96a81da3d85a18441
Davi Alves Oliveira <davi-ao@users.noreply.github.com>
2024-11-13 11:21:30 -0300

    1.1.0 - -nyms networks

--- wordnet_english_synonyms_hypernyms.R
+++ wordnet_english_synonyms_hypernyms.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(textstem)
 library(wordnet)
+library(igraph)
 
 setDict('wn3.1.dict/dict/')
 
-# Source of list of english words: 
+# Source of list of English words: 
 # Balota, D.A., Yap, M.J., Cortese, M.J., Hutchison, K.A., Kessler, B., Loftis, 
 # B., Neely, J.H., Nelson, D.L., Simpson, G.B., & Treiman, R. (2007). The 
 # English Lexicon Project. Behavior Research Methods, 39, 445-459.
@@ -58,3 +59,70 @@ results = read_csv('english_synonyms_hypernyms.csv') %>%
   filter(!(is.na(synonyms) & is.na(hypernyms)))
 
 write_csv(results, 'english_synonyms_hypernyms.csv')
+
+# Build -nyms networks
+
+nyms = read_csv('english_synonyms_hypernyms.csv',
+                show_col_types = F) %>%
+  pivot_longer(c(synonyms, hypernyms), 
+               names_to = 'relation', 
+               values_to = 'nyms') %>%
+  select(-relation) %>%
+  filter(!is.na(nyms)) %>%
+  separate_rows(nyms, sep = '\\|') %>%
+  filter((!nyms %>% str_detect('\\s')) & 
+           lemma != nyms) %>%
+  unique() %>%
+  ungroup()
+
+nyms_network_lemmas = nyms %>%
+  select(lemma, nyms) %>%
+  mutate(lemma = lemma %>% str_to_lower(),
+         nyms = nyms %>% str_to_lower()) %>%
+  unique() %>%
+  filter(lemma != '' & nyms != '' & lemma != nyms) %>%
+  as.matrix() %>% 
+  graph_from_edgelist(directed = F) %>%
+  simplify()
+
+V(nyms_network_lemmas)$id = V(nyms_network_lemmas)$name
+
+nyms_network_lemmas %>%
+  igraph::as_data_frame('vertices') %>%
+  write_csv('network_nyms_lemmas_vertices.csv')
+
+nyms_network_lemmas %>%
+  igraph::as_data_frame('edges') %>%
+  rename(Source = from,
+         Target = to) %>%
+  mutate(type = 'Undirected') %>%
+  write_csv('network_nyms_lemmas_edges.csv')
+
+nyms_network_lemmas %>%
+  write_graph('nyms_lemmas.net', 'pajek')
+
+nyms_network_stems = nyms %>%
+  select(lemma, nyms) %>%
+  mutate(lemma = lemma %>% str_to_lower() %>% stem_words(),
+         nyms = nyms %>% str_to_lower() %>% stem_words()) %>%
+  unique() %>%
+  filter(lemma != '' & nyms != '' & lemma != nyms) %>%
+  as.matrix() %>% 
+  graph_from_edgelist(directed = F) %>%
+  simplify()
+
+V(nyms_network_stems)$id = V(nyms_network_stems)$name
+
+nyms_network_stems %>%
+  igraph::as_data_frame('vertices') %>%
+  write_csv('network_nyms_stems_vertices.csv')
+
+nyms_network_stems %>%
+  igraph::as_data_frame('edges') %>%
+  rename(Source = from,
+         Target = to) %>%
+  mutate(type = 'Undirected') %>%
+  write_csv('network_nyms_stems_edges.csv')
+
+nyms_network_stems %>%
+  write_graph('nyms_stems.net', 'pajek')
